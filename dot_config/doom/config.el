75d231723c9c907e8817bff4b2845938061b0e7b
Matt Roman <mattroman@Christophers-MacBook-Pro.local>
2025-01-22 17:32:18 -0500

    Fix typo in font statement.

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -1,5 +1,5 @@
 (setq doom-theme 'doom-solarized-light
-      doom-font (font-spec :famly "Victor Mono" :size 16 )
+      doom-font (font-spec :family "Victor Mono" :size 16 )
       doom-big-font (font-spec :family "Victor Mono" :size 24))
 
 (setq display-line-numbers-type t)
@@ -19,15 +19,15 @@
 
 
 
-;; Custom styles for dates in agenda
-(custom-set-faces!
-  '(org-agenda-date :inherit outline-1 :height 1.15)
-  '(org-agenda-date-today :inherit outline-2 :height 1.15)
-  '(org-agenda-date-weekend :inherit outline-1 :height 1.15)
-  '(org-agenda-date-weekend-today :inherit outline-2 :height 1.15)
-  '(org-super-agenda-header :inherit custom-button :weight bold :height 1.05)
-  `(link :foreground unspecified :underline nil :background ,(nth 1 (nth 7 doom-themes--colors)))
-  '(org-link :foreground unspecified))
+  ;; Custom styles for dates in agenda
+  (custom-set-faces!
+    '(org-agenda-date :inherit outline-1 :height 1.15)
+    '(org-agenda-date-today :inherit outline-2 :height 1.15)
+    '(org-agenda-date-weekend :inherit outline-1 :height 1.15)
+    '(org-agenda-date-weekend-today :inherit outline-2 :height 1.15)
+    '(org-super-agenda-header :inherit custom-button :weight bold :height 1.05)
+    `(link :foreground unspecified :underline nil :background ,(nth 1 (nth 7 doom-themes--colors)))
+    '(org-link :foreground unspecified))
 
   ;; Enable org-modern mode for Org buffers
   (use-package! org-modern
@@ -44,131 +44,131 @@
             ("IN-PROGRESS" . "yellow"))))
 
   (setq org-agenda-category-icon-alist
-      `(
-        ("home" ,(list (all-the-icons-faicon "home" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
-        ("work" ,(list (all-the-icons-faicon "building" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
-        ("computer" ,(list (all-the-icons-faicon "laptop" :height 0.9 :v-adjust 0.01)) nil nil :ascent center)
-        ("errands" ,(list (all-the-icons-faicon "car" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
-        ("phone" ,(list (all-the-icons-faicon "phone" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
-   ))
+        `(
+          ("home" ,(list (all-the-icons-faicon "home" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
+          ("work" ,(list (all-the-icons-faicon "building" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
+          ("computer" ,(list (all-the-icons-faicon "laptop" :height 0.9 :v-adjust 0.01)) nil nil :ascent center)
+          ("errands" ,(list (all-the-icons-faicon "car" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
+          ("phone" ,(list (all-the-icons-faicon "phone" :height 0.9 :v-adjust 0.005)) nil nil :ascent center)
+          ))
 
 
   (setq org-agenda-prefix-format '(
- (agenda . "  %?-2i %s %t")
- (todo . " %?-3i  ")
- (tags . " %i %-12:c")
- (search . " %i %-12:c")))
+                                   (agenda . "  %?-2i %s %t")
+                                   (todo . " %?-3i  ")
+                                   (tags . " %i %-12:c")
+                                   (search . " %i %-12:c")))
 
-;; Optional: Enable for agenda
-(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  ;; Optional: Enable for agenda
+  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
   (setq org-agenda-files
-      '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"
-        "~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"
-        "~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org"))
+        '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"
+          "~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"
+          "~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org"))
 
   (setq org-refile-targets
-      '(("~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org" :maxlevel . 2)  ;; Refile to headings up to level 2 in projects.org
-        ("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org" :maxlevel . 2)
-        ("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org" :level . 1)
-        ("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org" :level . 1)))    ;; Refile to level 1 headings in someday.org
+        '(("~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org" :maxlevel . 2)  ;; Refile to headings up to level 2 in projects.org
+          ("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org" :maxlevel . 2)
+          ("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org" :level . 1)
+          ("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org" :level . 1)))    ;; Refile to level 1 headings in someday.org
 
-(setq org-capture-templates
-      '(("t" "Todo" entry
-         (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org" "Tasks")
-         "* TODO %?\n  %i\n  %a")
+  (setq org-capture-templates
+        '(("t" "Todo" entry
+           (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org" "Tasks")
+           "* TODO %?\n  %i\n  %a")
 
-        ("o" "One-on-One Meeting Topic" entry
-         (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org" "Topics")
-         "* [ ] %?\n  %i\n  Added: %U")
+          ("o" "One-on-One Meeting Topic" entry
+           (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org" "Topics")
+           "* [ ] %?\n  %i\n  Added: %U")
 
-        ("p" "Project" entry
-         (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org" "Active Projects")
-         "* %? :project:\n  %i\n  Created: %U")))
+          ("p" "Project" entry
+           (file+headline "~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org" "Active Projects")
+           "* %? :project:\n  %i\n  Created: %U")))
 
   (setq org-agenda-custom-commands
-      '(("i" "Inbox"
-         todo ""
-         ((org-agenda-files
-           '("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org"))
-          (org-agenda-overriding-header "Inbox Items")))
+        '(("i" "Inbox"
+           todo ""
+           ((org-agenda-files
+             '("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org"))
+            (org-agenda-overriding-header "Inbox Items")))
 
-        ("n" "Next Items"
-         todo ""
-         ((org-agenda-files
-           '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"))
-          (org-agenda-overriding-header "Next Actions")))
+          ("n" "Next Items"
+           todo ""
+           ((org-agenda-files
+             '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"))
+            (org-agenda-overriding-header "Next Actions")))
 
-        ("W" "Next @work"
-         ((agenda ""
-                  ((org-agenda-span 'week)
-                   (org-agenda-overriding-header "Weekly Agenda")))
+          ("W" "Next @work"
+           ((agenda ""
+                    ((org-agenda-span 'week)
+                     (org-agenda-overriding-header "Weekly Agenda")))
 
-         (tags-todo "@work"
-         ((org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"))
-          (org-agenda-override-header "Next Actions @Work")))))
+            (tags-todo "@work"
+                       ((org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org"))
+                        (org-agenda-override-header "Next Actions @Work")))))
 
-        ("d" "Daily Agenda"
-         ((agenda "" ((org-agenda-span 'day)
-                      (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
-                      (org-agenda-override-header "Next Actions @Work")
-                      (org-deadline-warning-days 7))))
-          (todo ""
-                     ((org-agenda-overriding-header "Tasks Without Due or Scheduled Dates")
-                      (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
-                  agenda-sorting-strategy '(priority-down)
-                      (org-agenda-skip-function
-                       '(org-agenda-skip-entry-if 'scheduled 'deadline)))))))
+          ("d" "Daily Agenda"
+           ((agenda "" ((org-agenda-span 'day)
+                        (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
+                                          (org-agenda-override-header "Next Actions @Work")
+                                          (org-deadline-warning-days 7))))
+            (todo ""
+                  ((org-agenda-overriding-header "Tasks Without Due or Scheduled Dates")
+                   (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
+                                     agenda-sorting-strategy '(priority-down)
+                                     (org-agenda-skip-function
+                                      '(org-agenda-skip-entry-if 'scheduled 'deadline)))))))
 
-        ("w" "Weekly Agenda"
-         ((agenda "" ((org-agenda-span 'week)
-                      (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
-                (org-agenda-override-header "Next Actions @Work")
-                      (org-deadline-warning-days 7))))
-          (todo ""
-                     ((org-agenda-overriding-header "Tasks Without Due or Scheduled Dates")
-                      (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
-                      (org-agenda-sorting-strategy '(priority-down)
-                      (org-agenda-skip-function
-                       '(org-agenda-skip-entry-if 'scheduled 'deadline))))))))
+          ("w" "Weekly Agenda"
+           ((agenda "" ((org-agenda-span 'week)
+                        (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
+                                          (org-agenda-override-header "Next Actions @Work")
+                                          (org-deadline-warning-days 7))))
+            (todo ""
+                  ((org-agenda-overriding-header "Tasks Without Due or Scheduled Dates")
+                   (org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/next.org")
+                                     (org-agenda-sorting-strategy '(priority-down)
+                                                                  (org-agenda-skip-function
+                                                                   '(org-agenda-skip-entry-if 'scheduled 'deadline))))))))
 
-        ("o" "One-on-Ones"
-         ((tags-todo ""
-                     ((org-agenda-files
-                       '("~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org"))
-                      (org-agenda-overriding-header "Open Topics")
-                      ))
+          ("o" "One-on-Ones"
+           ((tags-todo ""
+                       ((org-agenda-files
+                         '("~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org"))
+                        (org-agenda-overriding-header "Open Topics")
+                        ))
 
-          (tags ""
-                ((org-agenda-files
-                  '("~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org"))
-                 (org-agenda-overriding-header "Completed Topics")
-                 (org-agenda-skip-function
-                  '(org-agenda-skip-entry-if 'notregexp "\\[X\\]"))
-                 ))))
+            (tags ""
+                  ((org-agenda-files
+                    '("~/Library/Mobile Documents/com~apple~CloudDocs/org/one-on-ones.org"))
+                   (org-agenda-overriding-header "Completed Topics")
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\[X\\]"))
+                   ))))
 
 
-        ("p" "Planning"
-         ((todo ""
-         ((org-agenda-files
-           '("~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org"))
-          (org-agenda-overriding-header "Projects")))
-         (todo ""
-         ((org-agenda-files
-           '("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"))
-          (org-agenda-overriding-header "Someday")))
-    (tags-todo "-{.*}"
-                     ((org-agenda-overriding-header "Untagged Tasks")))
-    (todo "" ((org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org"))
+          ("p" "Planning"
+           ((todo ""
+                  ((org-agenda-files
+                    '("~/Library/Mobile Documents/com~apple~CloudDocs/org/projects.org"))
+                   (org-agenda-overriding-header "Projects")))
+            (todo ""
+                  ((org-agenda-files
+                    '("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"))
+                   (org-agenda-overriding-header "Someday")))
+            (tags-todo "-{.*}"
+                       ((org-agenda-overriding-header "Untagged Tasks")))
+            (todo "" ((org-agenda-files '("~/Library/Mobile Documents/com~apple~CloudDocs/org/inbox.org"))
                       (org-agenda-overriding-header "Unprocessed Inbox Items")))))
 
-        ("s" "Someday"
-         todo ""
-         ((org-agenda-files
-           '("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"))
-          (org-agenda-overriding-header "Someday Maybe")))))
+          ("s" "Someday"
+           todo ""
+           ((org-agenda-files
+             '("~/Library/Mobile Documents/com~apple~CloudDocs/org/someday.org"))
+            (org-agenda-overriding-header "Someday Maybe")))))
 
-)
+  )
 
 (after! org-roam
   (setq org-roam-directory (file-truename "~/Library/Mobile Documents/com~apple~CloudDocs/.hidden/roamnotes"))
@@ -180,7 +180,7 @@
         (concat "${title:*} "
                 (propertize "${tags:10}" 'face 'org-tag)))
 
-    ;; Define the capture templates
+  ;; Define the capture templates
   (setq org-roam-capture-templates
         '(("d" "default" plain
            "%?"
@@ -204,10 +204,10 @@
            :unnarrowed t)))
 
   (setq org-roam-dailies-capture-templates
-      '(("d" "default" entry
-         "* Today's Items\n\n** TODO %?\n\n* Today's Notes\n\n**\n"
-         :if-new (file+head "%<%Y-%m-%d>.org"
-                            "#+title: %<%Y-%m-%d>\n"))))
+        '(("d" "default" entry
+           "* Today's Items\n\n** TODO %?\n\n* Today's Notes\n\n**\n"
+           :if-new (file+head "%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n"))))
   (map! :leader
         :prefix "n"
         :desc "Toggle Org-roam buffer" "l" #'org-roam-buffer-toggle
