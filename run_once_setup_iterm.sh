cedabe4eb04f043eaa84b324416e9c81de1667e8
Matt Roman <mattroman@Christophers-MacBook-Pro.local>
2025-01-22 10:23:48 -0500

    fix the iterm2 install check

--- run_once_setup_iterm.sh
+++ run_once_setup_iterm.sh
@@ -2,8 +2,8 @@
 
 . ./setup.sh
 # Check if iTerm is installed
-if check_command_installed "open -a iTerm"; then
-    echo "Skipping asdf installation."
+if [ -d "/Applications/iTerm.app" ]; then
+    echo "Skipping iterm installation."
 else
     brew install --cask iterm2
 fi
